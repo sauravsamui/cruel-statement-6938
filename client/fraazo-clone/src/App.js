af676b46bea0f355705ead3f902565b29bbbbd9d
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-08-24 23:42:02 +0530

    Completed Navbar Footer

--- client/fraazo-clone/src/App.js
+++ client/fraazo-clone/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Navbar';
+import Footer from './Component/Footer';
+import DownloadApp from './Component/DownloadApp';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <DownloadApp/>
+     <Footer/>
     </div>
   );
 }
