5978569ac18fdde06e5ebff7a154695ddd51255e
sauravsamui <samuisaurav1@gmail.com>
2022-08-26 22:17:34 +0530

    order routes and others route update

--- server/Products/index.js
+++ server/Products/index.js
@@ -3,17 +3,22 @@ const connection = require("./db");
 const cors = require("cors");
 const cartRoute = require("./routes/cart.route");
 const productsRoute = require("./routes/products.routes");
+const  orderRoute  = require("./routes/orders.route");
 
 const app = express();
 require("dotenv").config()
 const PORT = process.env.PORT
+
 app.use(cors());
+
 app.use(express.json());
 app.use(express.text());
 
 app.use("/products",productsRoute);
 
 app.use("/carts",cartRoute);
+
+app.use("/orders",orderRoute);
 
 app.get("/",(req,res)=>{
 
--- server/Products/routes/cart.route.js
+++ server/Products/routes/cart.route.js
@@ -6,7 +6,8 @@ const ProductModel = require("../model/products.model");
 
 const cartRoute = Router();
 //http://localhost:8080/cart/items?user=5443664636&id=3545345345
-cartRoute.post("/items/:id",async(req,res)=>{
+// to add item to cart
+cartRoute.post("/post/:id",async(req,res)=>{
     let {user,id}=req.query;
     let payload = await ProductModel.findbyId({_id:id},{_id:0,benefits:0,description:0,category:0})
      payload = {
@@ -14,24 +15,37 @@ cartRoute.post("/items/:id",async(req,res)=>{
         user
     }
     // let f = await CartModel.find().populate("user");
-    let data = await CartModel.insertMany(body);
+    let data = await CartModel.create(body);
      res.send({message:"got it ",data});
 });
 
-cartRoute.get("/items/:id", async (req, res) => {
+// to get the cart items of user based on user id
+cartRoute.get("/get/:id", async (req, res) => {
     const user = req.params.id
     const data = await CartModel.find({user})
     res.send({message:"cart items",data})
 });
 
-cartRoute.delete("/items/:id", async (req, res) => {
+//to remove item from cart
+cartRoute.delete("/deleteone/:id", async (req, res) => {
     const id = req.params.id;
-    const data = await CartModel.deleteMany({$or:[{_id:id},{user:id}]});
+    const data = await CartModel.deleteMany({_id:id});
     res.send({message:"item deleted"});
 
 });
 
-cartRoute.patch("/items/:id", async (req, res) => {
+//to remove all the item from cart and add to order collection
+cartRoute.delete("/deletemany/:id", async (req, res) => {
+    const user = req.params.id;
+    let items = await CartModel.find({user},{_id:0})
+    await OrderModel.insertMany(items);
+    await CartModel.deleteMany({user});
+    res.send({message:"all item deleted and added to cart"});
+
+});
+
+// to update the quantity of item 
+cartRoute.patch("/patch/:id", async (req, res) => {
     const id = req.params.id
      const {quantity} = req.body;
     const data = await CartModel.updateOne({_id:id},{$set:{quantity:quantity}})
