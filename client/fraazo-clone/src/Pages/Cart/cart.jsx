d724a22c02d1673e46fab6ff84beeed30e452df5
Prakarshprasar <prakprasar@gmail.com>
2022-08-28 01:57:21 +0530

    update

--- client/fraazo-clone/src/Pages/Cart/cart.jsx
+++ client/fraazo-clone/src/Pages/Cart/cart.jsx
@@ -13,13 +13,7 @@ export const Cart = ({cartflag,setcartflag}) => {
   }
     const dispatch = useDispatch();
    const {cartData} = useSelector((state)=>state.cart);
-  // const arr=[
-  //   {id:1,src:"https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/231/original/data?width=256&height=256&format=webp",name:"Sun Melon",quant:"1pc",price:31,c:1},
-  //   {id:2,src:"https://imagemaster.fraazo.com/fraazo-master/products/FBAN14.png?width=256&height=256&format=webphttps://imagemaster.fraazo.com/fraazo-master/products/FBAN14.png?width=256&height=256&format=webp",name:"Robusta Banana",quant:"1kg",price:42,c:1},  
-   
-  // ]
-  //console.log(cartData);
-  // localStorage.setItem("arr",JSON.stringify(arr))
+  
   const [data, setdata] = useState();
   
 useEffect(() => {
